0deef617c620f48e5ab3a6ae03cb440331572586
Yatharth Singh <121056477+lefteryx@users.noreply.github.com>
2023-01-16 20:34:51 +0530

    correct import reference

--- DVM_T2_Django/DVM_T2_Django/DjangoQuiz/urls.py
+++ DVM_T2_Django/DVM_T2_Django/DjangoQuiz/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Quiz.views import *
+from DVM_T2_Django.Quiz.views import *
 from django.conf import settings
 from django.conf.urls.static import static
 
